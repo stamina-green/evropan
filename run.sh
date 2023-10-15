e91fc5338c0ab3a1c346af4080141303329b39bd
Matej Holubec <36100495+galook@users.noreply.github.com>
2023-10-15 15:12:32 +0200

    fix: script

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 git pull;
 npm i;
-if [ $1 = "vpn" ]; then
+if [ $1 == "vpn" ]; then
   nordvpn connect Dedicated_IP;
 fi
 x=1;
-while [ $x -le 7 ]; do sudo ts-node src/index.ts; done;
+while [ $x != 203 ]; do x=sudo ts-node src/index.ts; done;
